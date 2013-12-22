3c9d2bcf5c8b10589e8be9cb0bd187bf24fb2a13
Dobromir Ivanov <dobromir0ivanov@gmail.com>
2013-12-22 18:13:28 +0000

    merging into one file

--- lib/authstrategies.rb
+++ lib/authstrategies.rb
@@ -1,15 +1,97 @@
 require "authstrategies/version"
+require "warden"
+require "rack-flash"
+require "sinatra/base"
+require "activerecord"
+require "authstrategies/models/User"
+require "authstrategies/helpers"
 
 module Authstrategies
+	private
+		class Warden::SessionSerializer
+			def serialize(user)
+				user.id
+			end
 
+			def deserialize(id)
+				User.find(id)
+			end
+		end
+
+		class PasswordStrategy < Warden::Strategies::Base
+			def valid?
+				!!(request["email"] && request["password"])
+			end
+
+			def authenticate!
+				user = User.find_by_email(request["email"])
+				if user && user.authenticate(request)
+					success!(user)
+				else
+					fail!("Invalid username or password")
+				end
+			end
+		end
+
+		module Base
+			def self.registered(app)
+				app.helpers Helpers
+				app.use Warden::Manager do |manager|
+					manager.failure_app = app
+					manager.default_strategies :password
+				end
+
+				Warden::Strategies.add(:password, Strategies::PasswordStrategy)
+
+				app.get '/login/?' do
+					redirect '/' if authenticated?
+					erb :login
+				end
+
+				app.post '/login' do
+					redirect '/' if authenticated?
+					authenticate!
+					if authenticated?
+						flash[:notice] = "Logged in successfully!"
+						redirect '/'
+					else
+						flash[:error] = env["warden"].message
+						redirect '/login'
+					end
+				end
+
+				app.get '/logout/?' do
+					logout
+					flash[:notice] = "Successfully logged out!"
+					redirect '/'
+				end
+
+				app.post '/unauthenticated' do
+					flash[:error] = "Invalid username or password!"
+					redirect '/login'
+				end
+
+				app.get '/signup/?' do
+					redirect '/' if authenticated?
+					erb :signup
+				end
+
+				app.post '/signup' do
+					redirect '/' if authenticated?
+					user = User.new(params)
+					if user.valid?
+						user.save
+						flash[:notice] = "Successfully signed up!"
+						redirect '/'
+					else
+						flash[:error] = user.errors.messages
+						redirect '/login'
+					end
+				end
+			end
+		end
+
+		class Middleware < Sinatra::Base
+			register Base
+		end
 end
-
-require 'authstrategies/password_strategy'
-require 'authstrategies/base'
-require 'authstrategies/helpers'
-require 'authstrategies/routes'
-require 'authstrategies/failure_app'
-require 'authstrategies/config'
-require 'authstrategies/middleware'
-require 'authstrategies/session_serializer'
-require 'models/User'
--- lib/authstrategies/middleware.rb
+++
@@ -1,8 +0,0 @@
-require 'sinatra/base'
-
-module Authstrategies
-	private
-		class Middleware < Sinatra::Base
-			register Base
-		end
-end
