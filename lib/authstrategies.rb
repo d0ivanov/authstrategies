2f3dab8973b31abbc247e56bb5f64ccc25669c64
Dobromir Ivanov <dobromir0ivanov@gmail.com>
2013-12-23 03:12:04 +0000

    try to create a session cookie

--- lib/authstrategies.rb
+++ lib/authstrategies.rb
@@ -81,11 +81,12 @@ module Authstrategies
 					(auth.winning_strategy.is_a?(PasswordStrategy) &&
 					 auth.params['remember_me'] == 'on')
 					user.remember_me!  # new token
-					Rack::Session::Cookie.new(app,
+					cookie = Rack::Session::Cookie.new(app,
 						:key => "authstrategies.remember",
 						:secret => BCrypt::Password.create(Time.now),
 						:expire_after => 7 * 24 * 3600
 					)
+					auth.env['authstrategies.remember'] = cookie
 					auth.env['authstrategies.remember']['token'] = user.remember_token
 				end
 			end
